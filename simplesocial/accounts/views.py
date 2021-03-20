b477eae5caefe6d7faf930ac8ce8e7e1b18c11af
janki1234 <jodhanijanki@gmail.com>
2021-03-20 15:22:54 +0530

    'first'

---
+++ simplesocial/accounts/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+from . import forms
+
+
+class SignUpView(CreateView):
+	
+	form_class=forms.UserCreateForm
+	success_url=reverse_lazy('login')
+	template_name='accounts/signup.html'
+
---
+++ simplesocial/groups/admin.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+
+# Register your models here.
+from . import models
+
+
+		
+
+
+admin.site.register(models.Group)
+admin.site.register(models.GroupMember)
+
+
+
+
+
