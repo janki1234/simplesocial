cafcf1a791e02c534bea5257897f7fb650e96f53
Jodhani Janki <61007403+janki1234@users.noreply.github.com>
2021-03-20 15:30:25 +0530

    Update settings.py

--- simplesocial/simplesocial/settings.py
+++ simplesocial/simplesocial/settings.py
@@ -19,8 +19,6 @@ TEMPLATES_DIR=os.path.join(BASE_DIR,'templates')
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '5mm6=&)yt&vw!g=n^%&e^$g3uds$9krndd*i6b0i=+p=9h=58m'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
